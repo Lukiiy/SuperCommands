e8d7b1b0cfc22a26db4fecb6bfb611f754366149
Lukiiy <lukiiy@proton.me>
2026-07-17 13:39:47 -0300

    Make first argument optional

--- src/SuperCommands/provided/Kill.cs
+++ src/SuperCommands/provided/Kill.cs
@@ -4,13 +4,11 @@ public sealed class Kill : Command
 {
     public override string Name => "kill";
     public override string Description => "Kills a player.";
-    public override string Usage => "/kill <player>";
+    public override string Usage => "/kill [player]";
 
     public override void Execute(string[] args)
     {
-        if (args.Length == 0) throw CommandException.Usage(this);
-
-        PlayerInfo player = Utils.GetPlayerByName(args[0]) ?? throw CommandException.PlayerNotFound(this, args[0]);
+        PlayerInfo player = Utils.GetPlayerByName(args[0]) ?? GameManager.LocalPlayerInfo;
 
         player.AsGolfer.ServerEliminate(EliminationReason.OutOfBounds);
         Utils.SendMessage($"Killed {player.name}.");
